ead1dc0cff006113b6bcadb20d2ca9c270df05b0
Fabio Araujo <araujosilvafabio@hotmail.com>
2022-09-17 22:41:35 -0300

    second commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+var dataAtual = new Date();
+var dia = dataAtual.getDate();
+var mes = (dataAtual.getMonth() + 1);
+var ano = dataAtual.getFullYear();
+var horas = dataAtual.getHours();
+var minutos = dataAtual.getMinutes();
 
+document.getElementById('date').innerHTML = (dia + "/0" + mes + "/" + ano + "   " + horas + ":" + minutos);
