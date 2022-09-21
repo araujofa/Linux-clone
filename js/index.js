8ac1def8b6b7f2483cad725ec3daebfb002d3c50
Fabio Araujo <araujosilvafabio@hotmail.com>
2022-09-21 12:11:03 -0300

    Atualizacao desktop

--- js/index.js
+++ js/index.js
@@ -10,3 +10,34 @@ document.getElementById('date').innerHTML = (dia + "/0" + mes + "/" + ano + "   
 function menu(){
     window.alert("aqui vai ser um toggle menu");
 }
+
+function alterarCor(){
+    var a = document.getElementById('cor-g');  
+    a.style.backgroundColor = 'orange';
+    
+
+    a.addEventListener('mouseout', sair);
+    function sair(){
+        a.style.background = 'none'; 
+    }
+
+    a.addEventListener('mouseenter', entrar);
+    function entrar(){
+        a.style.background = 'rgba(255, 255, 255, 0.2)'; 
+    }
+}
+
+function alterarCorTrash(){
+    var b = document.getElementById('cor-t');
+    b.style.backgroundColor = 'orange';
+
+    b.addEventListener('mouseout', sair);
+    function sair(){
+        b.style.background = 'none'; 
+    }
+
+    b.addEventListener('mouseenter', entrar);
+    function entrar(){
+        b.style.background = 'rgba(255, 255, 255, 0.2)'; 
+    }
+}
