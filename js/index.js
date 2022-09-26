6608ee1212d352da01e69314e7a115996fc5a8e5
Fabio Araujo <araujosilvafabio@hotmail.com>
2022-09-26 09:31:19 -0300

    atualizacao desktop

--- js/index.js
+++ js/index.js
@@ -16,20 +16,54 @@ function alterarCor(){
     a.style.backgroundColor = 'rgba(255, 88, 27, 0.493)';
     a.style.border = '0.2px solid rgba(255, 255, 255, 0.432)';
 
-}
+    var ab = document.getElementById('cor-t');
+    ab.style.background = 'none';
+    ab.style.border ='none';
 
-function mouseEnter() {
-    var a = document.getElementById('cor-g');
-    a.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-    a.style.border = 'none';
-}
+    var abc = document.getElementById('cor-f');
+    abc.style.background = 'none';
+    abc.style.border ='none';
 
+}
 
 function alterarCorTrash(){
     var b = document.getElementById('cor-t');
-    b.style.backgroundColor = 'orange';
+    b.style.backgroundColor = 'rgba(255, 88, 27, 0.493)';
+    b.style.border = '0.2px solid rgba(255, 255, 255, 0.432)';
 
     var ab = document.getElementById('cor-g');
     ab.style.background = 'none';
     ab.style.border ='none';
+
+    var abc = document.getElementById('cor-f');
+    abc.style.background = 'none';
+    abc.style.border ='none';
+}
+
+function alterarCorFolder(){
+    var c = document.getElementById('cor-f');
+    c.style.backgroundColor = 'rgba(255, 88, 27, 0.493)';
+    c.style.border = '0.2px solid rgba(255, 255, 255, 0.432)';
+
+    var ab = document.getElementById('cor-t');
+    ab.style.background = 'none';
+    ab.style.border ='none';
+
+    var abc = document.getElementById('cor-g');
+    abc.style.background = 'none';
+    abc.style.border ='none';
+}
+
+function desclick(){
+    var a = document.getElementById('cor-t');
+    a.style.background = 'none';
+    a.style.border ='none';
+
+    var b = document.getElementById('cor-g');
+    b.style.background = 'none';
+    b.style.border ='none';
+
+    var c = document.getElementById('cor-f');
+    c.style.background = 'none';
+    c.style.border ='none';
 }
